c0d9a067d8107f28f53b7600402ae4fc5af2cc27
Aishwarya8888 <71621825+Aishwarya8888@users.noreply.github.com>
2022-02-05 14:50:09 +0530

    Uploaded Project

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotforcovid-bot-92af-Web-Deploy.pubxml -p:Password=zwga86aXEw6nfhCZc5mPGcZ08kv5rK2TDt9fcXdhu39QH99y6c4xjJRXPfvz
+
